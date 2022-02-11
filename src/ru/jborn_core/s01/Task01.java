ad032096ab67536ce4e670452f71f3709734ebee
ice_squirrel <zaluckii98@gmail.com>
2022-02-11 05:37:31 +0300

    Strange changed + 1 insertion STPBCH!

--- src/ru/jborn_core/s01/Task01.java
+++ src/ru/jborn_core/s01/Task01.java
@@ -3,6 +3,6 @@ package ru.jborn_core.s01;
 public class Task01 {
 
     public static void main(String[] args) {
-	System.out.println("Hello World MATHERFUCKA!");
+	System.out.println("Hello World MATHERFUCKA! Stupid beach!");
     }
 }
